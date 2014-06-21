edf55c6b001475c46e6314014e27fdfba8cf272a
Paul M. Rodriguez <pmr@ruricolist.com>
2014-06-20 19:49:35 -0500

    Update method signatures.

--- transform.lisp
+++ transform.lisp
@@ -1,13 +1,13 @@
 (in-package #:html5-sax)
 
-(defmethod html5-parser:transform-html5-dom ((to-type (eql :xhtml)) node)
+(defmethod html5-parser:transform-html5-dom ((to-type (eql :xhtml)) node &key)
   (serialize-dom node (cxml:make-string-sink)))
 
-(defmethod html5-parser:transform-html5-dom ((to-type (eql :cxml-dom)) node)
+(defmethod html5-parser:transform-html5-dom ((to-type (eql :cxml-dom)) node &key)
   (serialize-dom node (cxml-dom:make-dom-builder)))
 
-(defmethod html5-parser:transform-html5-dom ((to-type (eql :cxml-stp)) node)
+(defmethod html5-parser:transform-html5-dom ((to-type (eql :cxml-stp)) node &key)
   (serialize-dom node (cxml-stp:make-builder)))
 
-(defmethod html5-parser:transform-html5-dom ((to-type (eql :cxml-xmls)) node)
+(defmethod html5-parser:transform-html5-dom ((to-type (eql :cxml-xmls)) node &key)
   (serialize-dom node (cxml-xmls:make-xmls-builder)))
